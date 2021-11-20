93740df2f490ff1eee4838519059c5237b247024
Felipe Machado <phelipecwb@gmail.com>
2021-11-20 14:11:42 -0300

    Ajusts and improvements

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,55 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import {  Link  } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-import api from '../../services/api';
+import api from "../../services/api";
 
-import logoImg from '../../assets/images/logo.svg'
-import landingImg from '../../assets/images/landing.svg'
-import studyIcon  from '../../assets/images/icons/study.svg'
-import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
-import purpleHeatIcon from '../../assets/images/icons/purple-heart.svg'
+import logoImg from "../../assets/images/logo.svg";
+import landingImg from "../../assets/images/landing.svg";
+import studyIcon from "../../assets/images/icons/study.svg";
+import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
+import purpleHeatIcon from "../../assets/images/icons/purple-heart.svg";
 
- import  './styles.css';
-function Landind(){
+import "./styles.css";
+function Landind() {
   const [totalConnections, setTotalConnections] = useState(0);
+
+  const getData = async () => {
+    await api.get("/connections").then(({ data }) => {
+      setTotalConnections(data.total);
+    });
+  };
+
   useEffect(() => {
-    api.get('/connections').then(response =>{
-      const { total } =response.data;
-
-      setTotalConnections(total);
-    }
-      )
-  }, [])
-
-
+    getData();
+  }, []);
 
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
-        <div  className="logo-container">
-        <img src={logoImg} alt="Proffy"/>
-        <h2>Sua plataforma de estudos online</h2>
+        <div className="logo-container">
+          <img src={logoImg} alt="Proffy" />
+          <h2>Sua plataforma de estudos online</h2>
         </div>
-        <img src={landingImg} alt="Plataforma de estudos" className="hero-image"/>
+        <img
+          src={landingImg}
+          alt="Plataforma de estudos"
+          className="hero-image"
+        />
         <div className="buttons-container">
           <Link to="/study" className="study">
-            <img src={studyIcon} alt="Estudar"/>
+            <img src={studyIcon} alt="Estudar" />
             Estudar
           </Link>
 
           <Link to="/give-classes" className="give-classes">
-            <img src={giveClassesIcon} alt="Dar Aulas"/>
+            <img src={giveClassesIcon} alt="Dar Aulas" />
             Dar aulas
           </Link>
         </div>
         <span className="total-connections">
-          Total de {totalConnections} conexões já realizadas <img src={purpleHeatIcon} alt="Coração Roxo"/>
+          Total de {totalConnections} conexões já realizadas{" "}
+          <img src={purpleHeatIcon} alt="Coração Roxo" />
         </span>
-
       </div>
     </div>
-  )
-  
+  );
 }
 
 export default Landind;
